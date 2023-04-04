9add2ce2595f0d32c81c4a3ce8c098e23dea32a2
Esterkxz <elii.esterisk@gmail.com>
2023-04-04 12:47:03 +0900

    Cost table updated Genshin 3.5 phase 3

--- data/rule.js
+++ data/rule.js
@@ -12,7 +12,7 @@ Rule data created by Esterisk (에스터1z / Ester1z) \n\
 * this file is rule data reference and basic standard.\n\
     ",
     "rule_title": "앙챔스 룰북",
-    "rule_version": 1.4,
+    "rule_version": 1.5,
 
     "sequence": null,
     "cost_amount": -1,
