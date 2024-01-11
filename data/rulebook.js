03530e3ac9ee6a2151ad1f02370867a2b9eccbbd
Esterkxz <elii.esterisk@gmail.com>
2024-01-11 14:08:51 +0900

    Removed unreleased characters from selection pool

--- data/rulebook.js
+++ data/rulebook.js
@@ -232,7 +232,7 @@ Rule data created by Esterisk (에스터1z / Ester1z) \n\
                 "yunjin": true,
                 "layla": true,
                 "yaoyao": true,
-                "gaming": true,
+                // "gaming": true,
             },
             "": ""
         },
@@ -324,7 +324,7 @@ Rule data created by Esterisk (에스터1z / Ester1z) \n\
                 "lyney": true,
                 "wriothesley": true,
                 "navia": true,
-                "liuyun": true,
+                // "liuyun": true,
                 },
             "": ""
         },
